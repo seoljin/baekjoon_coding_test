438c1746c9dbcbfed1c9f9593abec85020121236
seoljin <84133354+seoljin@users.noreply.github.com>
2023-08-16 20:40:43 +0900

    Update 스택.py

--- 백준/Silver/10828. 스택/스택.py
+++ 백준/Silver/10828. 스택/스택.py
@@ -1,11 +1,13 @@
+# 더 빠르고 효율적으로 풀 수 있는 방법이 있으면 좋겠다
+
 import sys
 
 stack = []
-n = int(input())
+n = int(input()) # 명령 횟수
 
 for i in range(n):
-  s = sys.stdin.readline().split()
-  if s[0] == 'push':
+  s = sys.stdin.readline().split() # for문을 통한 입력은 좋지 않다는 것을 알게 됨, 앞으로 자주 사용해보면서 익혀나갈 예정
+  if s[0] == 'push': # 코랩에서는 여기서 에러 발생
     stack.append(s[1])
   elif s[0] == 'pop':
     if len(stack)==0:
